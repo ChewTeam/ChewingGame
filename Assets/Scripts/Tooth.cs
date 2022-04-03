c957fd153d356314a7b4738200d35f13a28fde82
Ренат <syltan.100renrgen@gmail.com>
2022-04-03 15:38:26 +0300

    add move tooth

---
+++ Assets/Scripts/Tooth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooth : MonoBehaviour
+{
+    [SerializeField] private float _minPositionY;
+    [SerializeField] private float _maxPositionY;
+
+    [SerializeField] private bool _isSelected;
+
+    private float _newPositionY;
+    private float _oldPositonY;
+
+    private void Start()
+    {
+        _newPositionY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        if (_isSelected)
+        {
+            Move();
+        }
+    }
+
+    public void Enter()
+    {
+        _isSelected = true;
+    }
+
+    public void Exit()
+    {
+        _isSelected = false;
+    }
+
+    private void Move()
+    {
+        float mousePositionY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _newPositionY = mousePositionY;
+            _oldPositonY = transform.position.y;
+        }
+
+        transform.position = new Vector3(transform.position.x, _oldPositonY + mousePositionY - _newPositionY, transform.position.z);
+        float correctPositionY = Mathf.Clamp(transform.position.y, _minPositionY, _maxPositionY);
+        transform.position = new Vector3(transform.position.x, correctPositionY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/ToothSelector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothSelector : MonoBehaviour
+{
+    [SerializeField] private LayerMask _filter;
+    private Tooth _selectedTooth;
+    private void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (_selectedTooth == null)
+            {
+                if (Physics.Raycast(mouseRay, out hit, _filter))
+                {
+                    if (hit.collider.gameObject.TryGetComponent(out Tooth tooth))
+                    {
+                        _selectedTooth = tooth;
+                        _selectedTooth.Enter();
+                    }
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (_selectedTooth != null)
+            {
+                _selectedTooth.Exit();
+                _selectedTooth = null;
+            }
+        }
+    }
+}
