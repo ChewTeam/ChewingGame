ea8ed8c26730788bec9f0491c93e5e94eafec1b1
Nevalayshka <47535881+HeBoJl9IlIka@users.noreply.github.com>
2022-04-03 17:02:56 +0400

    removed dubbing

--- Assets/Scripts/Item.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-abstract public class Item : MonoBehaviour
-{
-    [SerializeField] private string _label;
-    [SerializeField] private float _delayDisable;
-}
